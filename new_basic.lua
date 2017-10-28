6ee79b02b36dd57eb90c5da936270bbd8b346fd2
Sam <sam.hirsch66@gmail.com>
2017-10-27 21:51:41 -0600

    Fitness exists

--- new_basic.lua
+++ new_basic.lua
@@ -1,20 +1,40 @@
 local g = golly()
+g.autoupdate(true)
 
--- local ov = g.overlay
--- ov("create 1000 1000")
+local ov = g.overlay
+ov("create 1000 1000")
 
--- local op = require "oplus"
+local op = require "oplus"
 
--- local fitness = 0
-
-local startTime = g.millisecs()
-while (g.millisecs() - startTime  < 300000) do		
-  if g.empty() then
-    break
+-- fitness is function of pop size vs. initial and area covered
+function calcFitness ()
+ local popSize = tonumber( g.getpop() )
+ local rectArr = g.getrect()
+ local area = 0
+ if #rectArr >= 1 then
+    area = rectArr[3] * rectArr[4]
   end
+  if startPop == 0 then
+    return 0
+  end
+  return (popSize * area) / startPop
+end
 
-  g.fit()
-  g.step()
-  
-  -- op.maketext("Fitness: " .. fitness, "log", "rgba 0 0 0 255")
+function run ()
+  local fitness = 0
+  startPop = tonumber( g.getpop() )
+
+  local startTime = g.millisecs()
+  -- five minutes = 300000
+  while (g.millisecs() - startTime  < 300000) do
+    if g.empty() then
+      break
+    end
+
+    g.fit()
+    g.step()
+
+    op.maketext( "Fitness: " .. calcFitness())
+    op.pastetext(0, 0)
+  end
 end
